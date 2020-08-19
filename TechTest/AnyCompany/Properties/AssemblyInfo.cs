9868c4e918346c522b91546f035ed8554ed8a41c
Damian Walkow <damian3626@outlook.com>
2020-08-19 22:44:33 +0200

    Thanks for the opportunity and the fun tech-test

--- TechTest/AnyCompany/Properties/AssemblyInfo.cs
+++ TechTest/AnyCompany/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Investec Bank")]
 [assembly: AssemblyProduct("AnyCompany")]
-[assembly: AssemblyCopyright("Copyright © Investec Bank 2018")]
+[assembly: AssemblyCopyright("Copyright © Investec Bank 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
